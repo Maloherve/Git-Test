e89da9fc9010e6d8cc039ad3cd1a171e10b46f32
Unknown <malo.herve@epfl.ch>
2018-05-21 16:58:41 +0200

    Mise a jour de pointeur

--- Recap/Pointeur.cc
+++ Recap/Pointeur.cc
@@ -2,10 +2,11 @@ using namespace std;
 
 #include <memory>
 #include <iostream>
+#include <vector>
 // Pointeur Intelligent : Pas besoin de delete (il s'en occupent )
 // unique_ptr : verifie qu'a tout moment, un seul pointeur pointe vers une instance. Doivent pointer vers des instances dynamiquement allouée.
 
-
+// delete pointeur : supprime la valeur pointée pour un pointeur ALLOUE DYNAMIQUEMENT
 
 
 
@@ -21,43 +22,31 @@ int main(){
 
   int element(25);
 
-  // unique_ptr<int> p_element(new int(65));
-  // cout << *p_element << endl;
+  unique_ptr<int> p_element(new int(element));
 
 
-  unique_ptr<int> p_element2(nullptr);
-  // p_element2 = new int(56);
+  vector<int*> liste;
+
 
   int a(71);
+  int* p_a(&a);
+  liste.push_back(p_a);
 
-  unique_ptr<int> p_a(&a);
+
+
+  cout << *liste[0] << endl;
   cout << *p_a << endl;
-  cout << a << endl;
+  cout << a << endl << endl;
 
-  *p_a += 9;
+  p_a = nullptr;
+  p_a.clear();
+
+  cout << *liste[0] << endl;
   cout << *p_a << endl;
-  cout << a << endl;
+  cout << a << endl << endl;
 
 
-  // unique_ptr<int> p_a(nullptr);
-  // p_a = &a;
-  // cout << "Test" << endl;
-  // cout << *p_a << endl;
-  // cout << a << endl;
-  //
-  // *p_a += 9;
-  // cout << *p_a << endl;
-  // cout << a << endl;
 
-
-  // int* p_a(nullptr);
-  // p_a = &a;
-  // cout << *p_a << endl;
-  // cout << a << endl;
-  //
-  // *p_a += 9;
-  // cout << *p_a << endl;
-  // cout << a << endl;
 
   return 0;
 }
